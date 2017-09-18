a1f7670f876ff92474daa362e77db7b1cb4b288f
Grzegorz Pawlak <kalejdoskopwroclaw@gmail.com>
2017-09-18 11:51:32 +0200

    add new file

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+function checkOnclickEvent(param) {
+  console.log(param);
+}
+var inputElem = document.getElementById('js-inputValue');
+var imageElem = document.getElementById('js-image');
+var list = document.getElementById('list')
+var add = document.getElementById('addElem')
+
+
+add.addEventListener('click', function() {
+	var element = document.createElement('li');
+	//var numbers = document.getElementsByTagName('li').length;
+	element.innerHTML = 'item ' + document.getElementsByTagName('li').length;
+	list.appendChild(element);
+})
+
+
+imageElem.addEventListener('click', function(e) {
+  checkOnclickEvent('image was clicked')
+});
+
+inputElem.addEventListener('keypress', function(e) {
+  e.target.value += ' test ';
+});
+
